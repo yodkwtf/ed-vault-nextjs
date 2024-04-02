2619aee84c0a2bc29efaccb99609523d3a08e30a
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-04-02 16:55:58 +0530

    nav items completed for big screen

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,16 +1,33 @@
 'use client';
 
 import { PRODUCT_CATEGORIES } from '@/config';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import NavItem from './NavItem';
+import { useOnClickOutside } from '@/hooks/use-on-click-outside';
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  const isAnyOpen = activeIndex !== null;
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') setActiveIndex(null);
+    };
+
+    document.addEventListener('keydown', handler);
+
+    return () => {
+      document.removeEventListener('keydown', handler);
+    };
+  }, []);
+
+  const navRef = useRef<HTMLDivElement | null>(null);
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, index) => {
         const isOpen = index === activeIndex;
-        const isAnyOpen = activeIndex !== null;
 
         const toggleOpen = () => {
           setActiveIndex(isOpen ? null : index);
