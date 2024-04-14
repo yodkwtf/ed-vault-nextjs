2267d76ceb297f7805281810193efb8614f502de
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-04-14 14:52:35 +0530

    loggers added back

--- src/server.ts
+++ src/server.ts
@@ -33,12 +33,12 @@ const startServer = async () => {
   app.use((req, res) => nextHandler(req, res));
 
   nextApp.prepare().then(() => {
-    // payload.logger.info('Next.js is ready');
+    payload.logger.info('Next.js is ready');
 
     app.listen(PORT, async () => {
-      // payload.logger.info(
-      //   `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
-      // );
+      payload.logger.info(
+        `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      );
     });
   });
 };
