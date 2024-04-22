5ce52b83338cf7721ed366dc585b122da8d3851e
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-04-23 03:15:21 +0530

    add to cart button - UI completed

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -63,92 +63,94 @@ const Page = async ({ params }: PageProps) => {
 
   return (
     <MaxWidthWrapper className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-        {/* Product Details */}
-        <div className="lg:max-w-lg lg:self-end">
-          <ol className="flex items-center space-x-2">
-            {BREADCRUMBS.map((breadcrumb) => (
-              <li key={breadcrumb.id}>
-                <div className="flex items-center text-sm">
-                  <Link
-                    href={breadcrumb.href}
-                    className="font-medium text-sm text-muted-foreground hover:text-gray-900"
-                  >
-                    {breadcrumb.name}
-                  </Link>
-                  {breadcrumb.id !== BREADCRUMBS.length && (
-                    <svg
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                      className="ml-2 h-5 w-5 flex-shrink-0 text-gray-300"
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          {/* Product Details */}
+          <div className="lg:max-w-lg lg:self-end">
+            <ol className="flex items-center space-x-2">
+              {BREADCRUMBS.map((breadcrumb) => (
+                <li key={breadcrumb.id}>
+                  <div className="flex items-center text-sm">
+                    <Link
+                      href={breadcrumb.href}
+                      className="font-medium text-sm text-muted-foreground hover:text-gray-900"
                     >
-                      <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
-                    </svg>
-                  )}
+                      {breadcrumb.name}
+                    </Link>
+                    {breadcrumb.id !== BREADCRUMBS.length && (
+                      <svg
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                        className="ml-2 h-5 w-5 flex-shrink-0 text-gray-300"
+                      >
+                        <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+                      </svg>
+                    )}
+                  </div>
+                </li>
+              ))}
+            </ol>
+
+            <div className="mt-4">
+              <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                {product.name}
+              </h1>
+            </div>
+
+            <section className="mt-4">
+              <div className="flex items-center">
+                <p className="font-medium text-gray-900">
+                  {formatPrice(product.price)}
+                </p>
+
+                <div className="ml-4 border-l text-muted-foreground border-gray-300 pl-4">
+                  {category}
                 </div>
-              </li>
-            ))}
-          </ol>
-
-          <div className="mt-4">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              {product.name}
-            </h1>
-          </div>
-
-          <section className="mt-4">
-            <div className="flex items-center">
-              <p className="font-medium text-gray-900">
-                {formatPrice(product.price)}
-              </p>
-
-              <div className="ml-4 border-l text-muted-foreground border-gray-300 pl-4">
-                {category}
               </div>
-            </div>
 
-            <div className="mt-4 space-y-6">
-              <p className="text-base text-muted-foreground">
-                {product.description}
-              </p>
-            </div>
+              <div className="mt-4 space-y-6">
+                <p className="text-base text-muted-foreground">
+                  {product.description}
+                </p>
+              </div>
 
-            <div className="mt-6 flex items-center">
-              <Check
-                aria-hidden="true"
-                className="h-5 w-5 flex-shrink text-green-500"
-              />
-              <p className="ml-2 text-sm text-muted-foreground">
-                Eligible for instant delivery
-              </p>
-            </div>
-          </section>
-        </div>
-
-        {/* Product Images */}
-        <div className="mt-10 lg:col-start-2 lg:row-start-2 lg:mt-0 lg:self-center">
-          <div className="aspect-square rounded-lg">
-            <ImageSlider urls={imageUrls} />
-          </div>
-        </div>
-
-        {/* Add to cart */}
-        <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
-          <div>
-            <div className="mt-10">
-              <AddToCartButton />
-            </div>
-
-            <div className="mt-4 text-center">
-              <div className="group inline-flex text-sm text-medium">
-                <Shield
+              <div className="mt-6 flex items-center">
+                <Check
                   aria-hidden="true"
-                  className="mr-2 h-5 w-5 flex-shrink-0 text-gray-400"
+                  className="h-5 w-5 flex-shrink text-green-500"
                 />
-                <span className="text-muted-foreground hover:text-gray-700">
-                  30 Day Return Guarantee
-                </span>
+                <p className="ml-2 text-sm text-muted-foreground">
+                  Eligible for instant delivery
+                </p>
+              </div>
+            </section>
+          </div>
+
+          {/* Product Images */}
+          <div className="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center">
+            <div className="aspect-square rounded-lg">
+              <ImageSlider urls={imageUrls} />
+            </div>
+          </div>
+
+          {/* Add to cart */}
+          <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
+            <div>
+              <div className="mt-10">
+                <AddToCartButton />
+              </div>
+
+              <div className="mt-4 text-center">
+                <div className="group inline-flex text-sm text-medium">
+                  <Shield
+                    aria-hidden="true"
+                    className="mr-2 h-5 w-5 flex-shrink-0 text-gray-400"
+                  />
+                  <span className="text-muted-foreground hover:text-gray-700">
+                    30 Day Return Guarantee
+                  </span>
+                </div>
               </div>
             </div>
           </div>
--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,5 +1,24 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Button } from './ui/button';
+
 const AddToCartButton = () => {
-  return <div>AddToCartButton</div>;
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsSuccess(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, [isSuccess]);
+
+  return (
+    <Button onClick={() => setIsSuccess(true)} size="lg" className="w-full">
+      {isSuccess ? 'Added!' : 'Add to cart'}
+    </Button>
+  );
 };
 
 export default AddToCartButton;
